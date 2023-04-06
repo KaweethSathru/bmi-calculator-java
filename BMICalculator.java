8c50c0f1ce2dd0fca1622af225f92daae35240ab
Kaweeth Sathru <129934628+KaweethSathru@users.noreply.github.com>
2023-04-06 16:11:59 +0530

    Add files via upload

---
+++ BMICalculator.java
@@ -0,0 +1,51 @@
+package subtopic01;
+
+import java.util.Scanner;
+
+public class BMICalculator {
+    
+    //This is Main method
+    
+    public static void main(String args[]){
+  
+        System.out.println("Hello World"); 
+        System.out.println("joke");
+        System.out.println("Memes");
+        System.out.println("Ranil");
+        System.out.println("Mahinda");
+        System.out.println("hgbf ");
+        
+        System.out.print("jhdhauy");
+        System.out.println("habfd");
+        System.out.println("Hello");
+        System.out.println("udud");
+        
+        
+        
+        //monwath print wenne na
+        
+        System.out.println("dfasf");
+        
+        /*yuhdi
+        *uhduoh
+        *uhudfah
+        */
+        System.out.print("hjuah");
+        /*fafaf
+        faff
+        fafaf
+        faffsdegs
+        awf
+        */
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
